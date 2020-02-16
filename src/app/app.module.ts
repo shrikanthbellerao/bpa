0a458282edd783bd8e83c9f269b8b1e1e442b036
a.mahajan1999@gmail.com <a.mahajan1999@gmail.com>
2020-02-16 11:42:37 +0530

    reusable button and dropdown components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgxPopper } from 'angular-popper';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { TimelineComponent } from './components/utils/timeline/timeline.component';
+import { ButtonComponent } from './components/utils/button/button.component';
+import { DropdownComponent } from './components/utils/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
@@ -17,13 +21,17 @@ import { TimelineComponent } from './components/utils/timeline/timeline.componen
     LoginComponent,
     DashboardComponent,
     SpinnerComponent,
-    TimelineComponent
+    TimelineComponent,
+    ButtonComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AngularFontAwesomeModule,
+    NgxPopper
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/utils/dropdown/dropdown.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css']
+})
+export class DropdownComponent implements OnInit {
+  @Input()
+  drp
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
