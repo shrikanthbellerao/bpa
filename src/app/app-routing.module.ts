8a521e2d5190621cebc38a694f7ed771b3657f53
Jaspreet <Jaspreet@cisco.com>
2020-03-02 17:41:30 +0530

    doing commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/utils/signup/signup.component';
+
 
 const routes: Routes = [
   {path:"", component:LoginComponent},
-  {path:"dashboard", component:DashboardComponent}
+  {path:"dashboard", component:DashboardComponent},
+  {path:"signup",component:SignupComponent}
 ];
 
 @NgModule({
