49b4d668179d143c6b02d2b8990a3840ec60d50c
lakshmiPriya99 <lp.velupula@gmail.com>
2020-03-03 14:55:24 +0530

    Refactored bpa.service to use different IP address

--- src/app/service/bpa.service.ts
+++ src/app/service/bpa.service.ts
@@ -1,114 +1,104 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { ToastrModule, ToastrService } from "ngx-toastr";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class BpaService {
 
-  constructor(private httpClient: HttpClient, private toastr: ToastrService) { }
+  // vmIPAddress: string = '10.81.59.209:9091'; // BAC
+  // nsoInstance: string = 'RTP-Core,nso5-lsa4-re'; // BAC
+  vmIPAddress: string = '10.83.34.65'; // ATT-M
+  nsoInstance: string = 'All'; // ATT-M
+
+  constructor(
+    private httpClient: HttpClient, 
+    private toastr: ToastrService
+  ) {}
 
   fnValidateLogin(base64Credential) {
-
     const httpHeaders = {
       headers: new HttpHeaders({
-        Accept: 'application/json',
-        Authorization: 'Basic ' + base64Credential
+        Accept: "application/json",
+        Authorization: "Basic " + base64Credential
       })
     };
 
-    const url: string = 'https://10.81.59.209:9091/bpa/api/v1.0/login';
+    const url: string = `https://${this.vmIPAddress}/bpa/api/v1.0/login`;
     const requestBody = {};
 
     return this.httpClient.post(url, requestBody, httpHeaders);
   }
-  
-  getActiveServices(){
-    const getToken = localStorage.getItem('accessToken');
+
+  getActiveServices() {
+    const getToken = localStorage.getItem("accessToken");
     const httpHeaders = {
-    headers: new HttpHeaders({
-    Accept: 'application/json',
-    Authorization : `Bearer ${getToken}`
-    })
+      headers: new HttpHeaders({
+        Accept: "application/json",
+        Authorization: `Bearer ${getToken}`
+      })
     };
-    const urlActive : string ='https://10.81.59.209:9091/bpa/api/v1.0/service-catalog/service-orders'
-    
+    const urlActive: string =
+      `https://${this.vmIPAddress}/bpa/api/v1.0/service-catalog/service-orders`;
+
     return this.httpClient.get(urlActive, httpHeaders);
-    }
-    // getServiceItem(){
-    //   const getToken = localStorage.getItem('accessToken');
-    //   const httpHeaders = {
-    //   headers: new HttpHeaders({
-    //   Accept: 'application/json',
-    //   Authorization : `Bearer ${getToken}`
-    //   })
-    //   };
-    //   const urlActive : string ='https://10.81.59.209:9091/bpa/api/v1.0/service-catalog/service-items?_page=1&_limit=20&status=Active&order=asc'
-      
-    //   return this.httpClient.get(urlActive, httpHeaders);
-    //   }
-    /* Use below approach to display Toastr from any component:
+  }
+
+  /* 
+    Use below approach to display Toastr from any component:
     1. In case of Success message: this.toastr.success(msg, 'Success!');
     2. In case of Error message: this.toastr.error(msg, 'Error!');
     3. In case of Warning message: this.toastr.warning(msg, 'Alert!');
     4. In case of Info message: this.toastr.info(msg, 'Info');
-    /* Toaster code this.bpaService.showSuccess('Login Successful!')*/
-    /* Toaster code this.bpaService.showError('Invalid Credentials')
-    this.bpaService.showWarning('Maximum Attempts Allowed is 5')*/
-    /* Toaster code this.bpaService.showInfo('Remember Next time')*/
     
+    Toaster code this.bpaService.showSuccess('Login Successful!')
+    Toaster code this.bpaService.showError('Invalid Credentials')
+    this.bpaService.showWarning('Maximum Attempts Allowed is 5')
+    Toaster code this.bpaService.showInfo('Remember Next time')
+  */
 
-  getServiceItems()
-  {
-    const getToken = localStorage.getItem('accessToken');
+  getServiceItems() {
+    const getToken = localStorage.getItem("accessToken");
     const httpHeaders = {
       headers: new HttpHeaders({
-        Accept: 'application/json',
+        Accept: "application/json",
         Authorization: `Bearer ${getToken}`
       })
     };
 
-    const url2: string = 'https://10.81.59.209:9091/bpa/api/v1.0/service-catalog/service-items?_page=1&_limit=20&status=Active&order=asc';
+    const url2: string =
+      `https://${this.vmIPAddress}/bpa/api/v1.0/service-catalog/service-items?_page=1&_limit=20&status=Active&order=asc`;
 
     return this.httpClient.get(url2, httpHeaders);
   }
-  getServiceCategory(){
-    const getToken = localStorage.getItem('accessToken');
+  getServiceCategory() {
+    const getToken = localStorage.getItem("accessToken");
     const httpHeaders = {
       headers: new HttpHeaders({
-        Accept: 'application/json',
+        Accept: "application/json",
         Authorization: `Bearer ${getToken}`
       })
     };
 
-    const url: string = 'https://10.81.59.209:9091/bpa/api/v1.0/service-catalog/service-categories?_page=1&_limit=200000';
+    const url: string =
+      `https://${this.vmIPAddress}/bpa/api/v1.0/service-catalog/service-categories?_page=1&_limit=200000`;
 
-    return this.httpClient.get(url, httpHeaders)
-  }  
-  getDeviceList()
-  {
-    const getToken = localStorage.getItem('accessToken');
+    return this.httpClient.get(url, httpHeaders);
+  }
+
+  getDeviceList() {
+    const getToken = localStorage.getItem("accessToken");
     const httpHeaders = {
       headers: new HttpHeaders({
-        Accept: 'application/json',
-        Authorization : `Bearer ${getToken}`
+        Accept: "application/json",
+        Authorization: `Bearer ${getToken}`
       })
     };
 
-    const urlDevices : string ='https://10.81.59.209:9091/bpa/api/v1.0/device-manager/devices?limit=5000&page=1&nsoInstance=RTP-Core,nso5-lsa4-re'
-    
+    const urlDevices: string =
+      `https://${this.vmIPAddress}/bpa/api/v1.0/device-manager/devices?limit=5000&page=1&nsoInstance=${this.nsoInstance}`;
+
     return this.httpClient.get(urlDevices, httpHeaders);
   }
-  /* Use below approach to display Toastr from any component:
-
-  1. In case of Success message: this.toastr.success(msg, 'Success!');
-  2. In case of Error message: this.toastr.error(msg, 'Error!');
-  3. In case of Warning message: this.toastr.warning(msg, 'Alert!');
-  4. In case of Info message: this.toastr.info(msg, 'Info');
-      /* Toaster code this.bpaService.showSuccess('Login Successful!')*/
-  /* Toaster code this.bpaService.showError('Invalid Credentials')
-this.bpaService.showWarning('Maximum Attempts Allowed is 5')*/
-  /* Toaster code this.bpaService.showInfo('Remember Next time')*/
-
 }
