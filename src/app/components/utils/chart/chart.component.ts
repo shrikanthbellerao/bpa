1ae2e2d6ff97abaf9d9d69cf6d80a6bebb306996
divyakumari32 <dkumaribe16@gmail.com>
2020-03-13 00:39:06 +0530

    Dashboard Changes

--- src/app/components/utils/chart/chart.component.ts
+++ src/app/components/utils/chart/chart.component.ts
@@ -14,100 +14,25 @@ export class ChartComponent implements OnInit {
   constructor() { }
 
   chart:any = [];
+  objservice = [];
   
+
   ngOnInit() {
-  
-    // this.chartElements.forEach((data) => {
-    //     if(data.categoryIds.name === "Refresh Cisco IP Phone")
-    //       this.collabCount++;
-    //       // if(this.flag1)
-    //       //  this.collabCount++;
-    //       // else 
-    //       //  this.collab1Count++;
-        
-    //       // this.flag1 = !this.flag1;
-        
-    //     if(data.categoryIds.name === "MVP Service Catalog Refresh #2")
-    //       this.mvpCount++;
-    //       // if(this.flag2)
-    //       //  this.mvpCount++;
-    //       // else
-    //       //  this.mvp1Count++;
 
-    //       //  this.flag2 = !this.flag2;
+    let counts = [];
+    this.chartElements.forEach((x) => { counts[x.categoryIds[0].name] = (counts[x.categoryIds[0].name] || 0)+1; });
     
-    //     if(data.categoryIds.name === "Enterprise Services")
-    //       this.enterpriseCount++;
-    //       // if(this.flag3)
-    //       //  this.enterpriseCount++;
-    //       // else
-    //       //  this.enterprise1Count++;
-
-    //       //  this.flag3 = !this.flag3;
-        
-        
-    //     if(data.categoryIds.name === "Core Services")
-    //       this.coreCount++;
-    //       // if(this.flag4)
-    //       //  this.coreCount++;
-    //       // else
-    //       //  this.core1Count++;
-
-    //       //  this.flag4 = !this.flag4;
-        
-         
-    //     if(data.categoryIds.name === "Branch Services")
-    //       this.branchCount++;
-    //       // console.log(this.branchCount);
-    //       // if(this.flag5)
-    //       //  this.branchCount++;
-    //       // else
-    //       //  this.branch1Count++;
-
-    //       // this.flag5 = !this.flag5;
-        
-        
-    //     if(data.categoryIds.name === "DMZ Services")
-    //       this.dmzCount++;
-    //       // if(this.flag6)
-    //       //  this.dmzCount++;
-    //       // else
-    //       //  this.dmz1Count++;
-
-    //       // this.flag6 = !this.flag6;
-        
-         
-    //     if(data.categoryIds.name === "Common Services")
-    //       this.commonCount++;
-    //       // if(this.flag7)
-    //       //  this.commonCount++;
-    //       // else
-    //       //  this.common1Count++;
-
-    //       // this.flag7 = !this.flag7;
-        
-         
-    //     if(data.categoryIds.name === "Data Center Services")
-    //       this.datacenterCount++;
-    //       // if(this.flag8)
-    //       //  this.datacenterCount++;
-    //       // else
-    //       //  this.datacenter1Count++;
-
-    //       // this.flag8 = !this.flag8;
-        
-    // })
-    
-  
+    console.log('counts',Object.keys(counts))
+    console.log('counts',[...Object.values(counts)].map((res) => (res/2 === 0) ? res/2 : res))
     
     this.chart = new Chart('canvas', {
       type: 'line',
       data: {
-        labels: ["Collaboration","MVP","Enterprise","Core","Branch","Common","Data Center"],
+        labels: Object.keys(counts),
         datasets: [
           { 
             label: "Admin",
-            data: [5,1,3,1,7,2,1,2],
+            data: Object.values(counts),
             borderColor: "#3cba9f",
             fill: false
           },
