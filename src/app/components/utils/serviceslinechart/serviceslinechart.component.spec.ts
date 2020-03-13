872de96efa320989529f944dde014de3551533cb
divyakumari32 <dkumaribe16@gmail.com>
2020-03-13 10:06:18 +0530

    material/tabs uninatalled

--- src/app/components/utils/serviceslinechart/serviceslinechart.component.spec.ts
+++ src/app/components/utils/serviceslinechart/serviceslinechart.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ChartComponent } from './chart.component';
+import { ServiceslinechartComponent } from './serviceslinechart.component';
 
 describe('ChartComponent', () => {
-  let component: ChartComponent;
-  let fixture: ComponentFixture<ChartComponent>;
+  let component: ServiceslinechartComponent;
+  let fixture: ComponentFixture<ServiceslinechartComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChartComponent ]
+      declarations: [ ServiceslinechartComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ChartComponent);
+    fixture = TestBed.createComponent(ServiceslinechartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
