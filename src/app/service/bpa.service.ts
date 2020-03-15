a37cf06fa89a143b9e2fa1162f7cdb07e341a784
divyakumari32 <dkumaribe16@gmail.com>
2020-03-15 13:18:30 +0530

    Conflict changes

--- src/app/service/bpa.service.ts
+++ src/app/service/bpa.service.ts
@@ -81,18 +81,6 @@ export class BpaService {
     return this.httpClient.get(urlActive, bpaHttpHeaders);
   }
 
-/* 
-  Use below approach to display Toastr from any component:
-  1. In case of Success message: this.toastr.success(msg, 'Success!');
-  2. In case of Error message: this.toastr.error(msg, 'Error!');
-  3. In case of Warning message: this.toastr.warning(msg, 'Alert!');
-  4. In case of Info message: this.toastr.info(msg, 'Info');
-  
-  Toaster code this.bpaService.showSuccess('Login Successful!')
-  Toaster code this.bpaService.showError('Invalid Credentials')
-  this.bpaService.showWarning('Maximum Attempts Allowed is 5')
-  Toaster code this.bpaService.showInfo('Remember Next time')
-*/
   // Method to get the list of Service Orders from Service Catalog microservice of BPA
   getServiceorders() {
 
