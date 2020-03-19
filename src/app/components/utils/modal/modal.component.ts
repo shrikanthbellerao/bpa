2cfaaea4ba4aaf6ce928749a7b9ce819383b6f3d
Jaspreet <Jaspreet@cisco.com>
2020-03-19 22:01:18 +0530

    commit done

--- src/app/components/utils/modal/modal.component.ts
+++ src/app/components/utils/modal/modal.component.ts
@@ -15,12 +15,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-modal',
-<<<<<<< HEAD
-  templateUrl: './modal.component.html'
-=======
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
->>>>>>> 9f89de9d390c9896ada73ed6c06759e148c970f7
 })
 
 export class ModalComponent implements OnInit {
@@ -29,12 +25,9 @@ export class ModalComponent implements OnInit {
 
   @Input() modalConfig: any;
 
-<<<<<<< HEAD
-=======
 
   @Input() timelineElement: any
   @Input() timelineElements: any
->>>>>>> 9f89de9d390c9896ada73ed6c06759e148c970f7
   @Output() closeModal = new EventEmitter();
 
   constructor() { }
@@ -43,11 +36,8 @@ export class ModalComponent implements OnInit {
   }
 
   hideModal() {
-<<<<<<< HEAD
     this.modalConfig.show=false;
     this.closeModal.emit()
-=======
     this.closeModal.emit();
->>>>>>> 9f89de9d390c9896ada73ed6c06759e148c970f7
   }
 }
