f8bf30c0a9183a5f95e160f98bd086c2bb471fff
lakshmiPriya99 <lp.velupula@gmail.com>
2020-03-27 10:32:30 +0530

    Designed Responsive Profile Page with API Calls

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BpaService } from 'src/app/service/bpa.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+  adminDetails: Object;
+  demoDetails: Object;
 
+  constructor(private bpaService: BpaService) { }
+  user;
   ngOnInit() {
+    this.bpaService.fnFetchAdminDetails().subscribe(res => {
+      console.log('Admin User Details fetched from backend', res);
+      this.adminDetails = res;      
+    }, err => console.log('Error:', err));
+  
+    this.bpaService.fnFetchDemoDetails().subscribe(res => {
+      console.log('Demo User Details fetched from backend', res);
+      this.demoDetails = res;
+    }, err => console.log('Error:', err));
+
+    this.user = localStorage.getItem("userdata");
   }
 
 }
