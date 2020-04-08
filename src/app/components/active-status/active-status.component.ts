b9626272464232b49bc4fdc8f9b411eb57832b2b
aaymahaj <a.mahajan1999@gmail.com>
2020-04-08 16:02:15 +0530

    aayushi's test cases

--- src/app/components/active-status/active-status.component.ts
+++ src/app/components/active-status/active-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { BpaService } from 'src/app/service/bpa.service';
 
 @Component({
@@ -7,7 +7,7 @@ import { BpaService } from 'src/app/service/bpa.service';
 })
 export class ActiveStatusComponent implements OnInit {
 
- 
+  @Input() timelineElement: any
   constructor(private dcService: BpaService) { }
   showSelectedData;
   ngOnInit() {
